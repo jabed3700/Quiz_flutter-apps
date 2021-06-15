14e8d9f206cb7633e8aef460e22592b8b13077d6
tamim08 <tamim008bd@gmail.com>
2021-06-15 09:18:30 +0600

    Abstraction in action

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
   runApp(MaterialApp(
@@ -398,6 +399,148 @@ void main() {
 // }
 
 // Creating a question class (8)
+// class QuizPage extends StatefulWidget {
+//   // const QuizPage({Key? key}) : super(key: key);
+//
+//   @override
+//   _QuizPageState createState() => _QuizPageState();
+// }
+//
+// class _QuizPageState extends State<QuizPage> {
+//   List<Icon> scoreKeeper = [
+//     Icon(
+//       Icons.check,
+//       color: Colors.green,
+//     ),
+//     Icon(
+//       Icons.close,
+//       color: Colors.red,
+//     ),
+//     Icon(
+//       Icons.close,
+//       color: Colors.red,
+//     ),
+//     Icon(
+//       Icons.close,
+//       color: Colors.red,
+//     ),
+//     Icon(
+//       Icons.check,
+//       color: Colors.green,
+//     ),
+//   ];
+//
+//   // List<String> questions = [
+//   //   'You can lead a cow down stairs but not up stairs',
+//   //   'Approximately one quarter of human bones are in the feet.',
+//   //   'A slug\'s blood is green.'
+//   // ];
+//   //
+//   // List<bool> answers = [false,true, true,];
+//   //
+//   // Question q1 = Question(q : 'You can lead a cow down stairs but not up stairs.', a:false);
+//
+//   List<Question> questionBank = [
+//     Question(q: 'You can lead a cow down stairs but not up stairs', a:false),
+//     Question(q: 'Approximately one quarter of human bones are in the feet.', a:true),
+//     Question(q: 'A slug\'s blood is green.',a:true),
+//   ];
+//
+//   int questionNumber = 0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       crossAxisAlignment: CrossAxisAlignment.stretch,
+//       children: [
+//         Expanded(
+//           flex: 5,
+//           child: Padding(
+//             padding: const EdgeInsets.all(10.0),
+//             child: Center(
+//               child: Text(
+//                 // questions[2],
+//                 questionBank[questionNumber].questionText,
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 25.0,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//         Expanded(
+//           child: Padding(
+//             padding: const EdgeInsets.all(15.0),
+//             child: FlatButton(
+//                 color: Colors.green,
+//                 padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+//                 onPressed: () {
+//                   bool correctAnswer = questionBank[questionNumber].questionAnswer;
+//                   if(correctAnswer == true){
+//                     print('user got it right!');
+//                   }else{
+//                     print('user got it wrong');
+//                   }
+//                   setState(() {
+//                     questionNumber++;
+//                   });
+//                   print(questionNumber);
+//                 },
+//                 child: Text(
+//                   'True',
+//                   style: TextStyle(fontSize: 20.0, color: Colors.white),
+//                 )),
+//           ),
+//         ),
+//         Expanded(
+//           child: Padding(
+//             padding: const EdgeInsets.all(15.0),
+//             child: FlatButton(
+//               color: Colors.red,
+//               onPressed: () {
+//                 bool correctAnswer = questionBank[questionNumber].questionAnswer;
+//                 if(correctAnswer == false){
+//                   print('user got it right!');
+//                 }else{
+//                   print('user got it wrong');
+//                 }
+//                 setState(() {
+//                   questionNumber++;
+//                 });
+//                 print(questionNumber);
+//                 // setState(() {
+//                 //   scoreKeeper.add(
+//                 //     Icon(
+//                 //       Icons.close,
+//                 //       color: Colors.red,
+//                 //     ),
+//                 //   );
+//                 // });
+//               },
+//               child: Text(
+//                 'False',
+//                 style: TextStyle(
+//                   fontSize: 20.0,
+//                   color: Colors.white,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//         Row(
+//           children: scoreKeeper,
+//         )
+//       ],
+//     );
+//   }
+// }
+
+
+// Abstraction in action
+// Creating a question class (8)
 class QuizPage extends StatefulWidget {
   // const QuizPage({Key? key}) : super(key: key);
 
@@ -407,26 +550,26 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
+    // Icon(
+    //   Icons.check,
+    //   color: Colors.green,
+    // ),
+    // Icon(
+    //   Icons.close,
+    //   color: Colors.red,
+    // ),
+    // Icon(
+    //   Icons.close,
+    //   color: Colors.red,
+    // ),
+    // Icon(
+    //   Icons.close,
+    //   color: Colors.red,
+    // ),
+    // Icon(
+    //   Icons.check,
+    //   color: Colors.green,
+    // ),
   ];
 
   // List<String> questions = [
@@ -439,11 +582,11 @@ class _QuizPageState extends State<QuizPage> {
   //
   // Question q1 = Question(q : 'You can lead a cow down stairs but not up stairs.', a:false);
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs', a:false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a:true),
-    Question(q: 'A slug\'s blood is green.',a:true),
-  ];
+  // List<Question> questionBank = [
+  //   Question(q: 'You can lead a cow down stairs but not up stairs', a:false),
+  //   Question(q: 'Approximately one quarter of human bones are in the feet.', a:true),
+  //   Question(q: 'A slug\'s blood is green.',a:true),
+  // ];
 
   int questionNumber = 0;
 
@@ -460,7 +603,7 @@ class _QuizPageState extends State<QuizPage> {
             child: Center(
               child: Text(
                 // questions[2],
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.white,
@@ -477,7 +620,7 @@ class _QuizPageState extends State<QuizPage> {
                 color: Colors.green,
                 padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
                 onPressed: () {
-                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                  bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                   if(correctAnswer == true){
                     print('user got it right!');
                   }else{
@@ -500,7 +643,7 @@ class _QuizPageState extends State<QuizPage> {
             child: FlatButton(
               color: Colors.red,
               onPressed: () {
-                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                 if(correctAnswer == false){
                   print('user got it right!');
                 }else{
--- lib/question.dart
+++ lib/question.dart
@@ -1,7 +1,19 @@
+// class Question{
+//   String questionText;
+//   bool questionAnswer;
+//   Question({String q,bool a}){
+//
+//     questionText=q;
+//     questionAnswer=a;
+//
+//   }
+// }
+
+
 class Question{
   String questionText;
   bool questionAnswer;
-  Question({String q,bool a}){
+  Question(String q,bool a){
 
     questionText=q;
     questionAnswer=a;
